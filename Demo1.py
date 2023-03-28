fb4c7c1272825264e40836397d10d9595723316c
Ugot, Aaron Paul M <123426481+Ugot-Aaron-Paul@users.noreply.github.com>
2023-03-28 08:55:14 +0800

    Add files via upload

---
+++ Demo1.py
@@ -0,0 +1,14 @@
+from tkinter import *
+window = Tk()
+
+window.geometry("500x400+10+10")
+window.title("My First Graphical User Interface")
+
+txt = Entry(window,border = 10)
+txt.place(x=170,y=150)
+btn = Button(text = "Click me",fg="red", bg = "yellow")
+btn.place(x=210,y=200)
+
+
+
+window.mainloop()
